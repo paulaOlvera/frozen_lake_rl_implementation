b5823a5725a557bc4280f89cdd1c8efa30697f1b
paula.olvera <olveralpaula@gmail.com>
2026-07-10 14:30:44 +0100

    adapts the training file to see if i'm doing well with the deep q learning agent

--- training.py
+++ training.py
@@ -1,25 +1,28 @@
 import numpy as np
-from agents import Q_learningAgent
+from agents import Q_learningAgent, Deep_Q_learningAgent
 from gridworld import GridWorld
 
 env = GridWorld(True)
-agent = Q_learningAgent()
-max_episodes = 100000
-episode = 0
-if __name__ == '__main__': 
-    # episodes for the training
-    while episode < max_episodes:
-        agent.epsilon = max(0.01, np.exp(-0.001 * episode))
-        while True: 
-            state = env.get_state()
-            action = agent.choose_action(state)
-            next_step,reward, done = env.step(action)
-            q_value_table = agent.update_q_values(state,reward,next_step,action)
-            print(episode)
-            env.render()
+deep_agent = Deep_Q_learningAgent()
+deep_agent.encoding_input(env)
+print(deep_agent)
+# agent = Q_learningAgent()
+# max_episodes = 100000
+# episode = 0
+# if __name__ == '__main__': 
+#     # episodes for the training
+#     while episode < max_episodes:
+#         agent.epsilon = max(0.01, np.exp(-0.001 * episode))
+#         while True: 
+#             state = env.get_state()
+#             action = agent.choose_action(state)
+#             next_step,reward, done = env.step(action)
+#             q_value_table = agent.update_q_values(state,reward,next_step,action)
+#             print(episode)
+#             env.render()
             
-            if done:
-                env.reset()
-                episode +=1
-                break
-    np.save("frozen_lake_rl_implementation/q_table.npy", agent.q_table)
+#             if done:
+#                 env.reset()
+#                 episode +=1
+#                 break
+#     np.save("frozen_lake_rl_implementation/q_table.npy", agent.q_table)
